fb2f109fd9535f4931def87a035f26bbcc6ff80d
Kevin <koderkk@gmail.com>
2017-03-25 22:03:53 -0400

    Fix it

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ enum Operator {
 enum Token {
     LeftDelimiter,
     RightDelimiter,
-    Operator(Operator, u32),
+    Operator{ operator: Operator, unary: bool, precedence: u32 },
     Number(f64),
     None
 }
@@ -26,8 +26,7 @@ fn is_numeric(c: char) -> bool {
 
 fn is_unary(last_token: &Token) -> bool {
     match *last_token {
-        Token::LeftDelimiter => true,
-        Token::Operator(_, _) => true,
+        Token::LeftDelimiter | Token::Operator{..} | Token::None => true,
         _ => false
     }
 }
@@ -45,16 +44,21 @@ fn push_token(token: Token, stack: &mut Vec<Token>, output: &mut Vec<Token>) {
             }
             panic!("Mismatched parentheses");
         },
-        Token::Operator(_, op_precedence) => {
-            while stack.len() > 0 {
-                match *stack.last().unwrap() {
-                    Token::LeftDelimiter | Token::RightDelimiter => break,
-                    Token::Operator(_, precedence) => if precedence < op_precedence { break },
-                    _ => {}
-                }
-                output.push(stack.pop().unwrap());
+        Token::Operator{ unary, precedence: op_precedence, .. } => {
+            if unary {
+                stack.push(token);
             }
-            stack.push(token);
+            else {
+                while stack.len() > 0 {
+                    match *stack.last().unwrap() {
+                        Token::LeftDelimiter | Token::RightDelimiter => break,
+                        Token::Operator{ precedence, .. } => if precedence < op_precedence { break },
+                        _ => {}
+                    }
+                    output.push(stack.pop().unwrap());
+                }
+                stack.push(token);
+            }
         },
         Token::Number(_) => output.push(token),
         _ => {}
@@ -88,14 +92,14 @@ fn parse(input: String) -> Vec<Token> {
             let token = match c {
                 '(' => Token::LeftDelimiter,
                 ')' => Token::RightDelimiter,
-                '+' => if !is_unary(&last_token) { Token::Operator(Operator::Add, 1) }
+                '+' => if !is_unary(&last_token) { Token::Operator{ operator: Operator::Add, unary: false, precedence: 1 } }
                        else { Token::None },
-                '-' => if is_unary(&last_token) { Token::Operator(Operator::Negate, 3) }
-                       else { Token::Operator(Operator::Subtract, 1) },
-                '/' => Token::Operator(Operator::Divide, 2),
-                '*' => Token::Operator(Operator::Multiply, 2),
-                '%' => Token::Operator(Operator::Mod, 2),
-                '^' => Token::Operator(Operator::Power, 4),
+                '-' => if is_unary(&last_token) { Token::Operator{ operator: Operator::Negate, unary: true, precedence: 3 } }
+                       else { Token::Operator{ operator: Operator::Subtract, unary: false, precedence: 1 } },
+                '/' => Token::Operator{ operator: Operator::Divide, unary: false, precedence: 2 },
+                '*' => Token::Operator{ operator: Operator::Multiply, unary: false, precedence: 2 },
+                '%' => Token::Operator{ operator: Operator::Mod, unary: false, precedence: 2 },
+                '^' => Token::Operator{ operator: Operator::Power, unary: false, precedence: 4 },
                 _ => panic!("Invalid character in expression")
             };
             last_token = token;
@@ -119,17 +123,42 @@ fn get_param(stack: &mut Vec<f64>) -> f64 {
 
 fn solve(tokens: Vec<Token>) -> f64 {
     let mut stack: Vec<f64> = Vec::new();
+
+    /*
+    println!("----------------------------------------------");
+    for token in &tokens {
+        match *token {
+            Token::Number(_) => println!("Number"),
+            Token::Operator{unary, ..} => println!("Operator {}", if unary { "Unary" } else { "Binary" }),
+            _ => println!("???")
+        }
+    }
+    println!("----------------------------------------------");
+    */
+
     for token in &tokens {
         let result: f64 = match *token {
             Token::Number(n) => n,
-            Token::Operator(operator, _) => match operator {
+            Token::Operator{ operator, .. } => match operator {
                 Operator::Add => get_param(&mut stack) + get_param(&mut stack),
-                Operator::Subtract => get_param(&mut stack) - get_param(&mut stack),
+                Operator::Subtract => {
+                    let n = get_param(&mut stack);
+                    get_param(&mut stack) - n
+                }
                 Operator::Multiply => get_param(&mut stack) * get_param(&mut stack),
-                Operator::Divide => get_param(&mut stack) / get_param(&mut stack),
-                Operator::Mod => ((get_param(&mut stack) as u64) % (get_param(&mut stack) as u64)) as f64,
+                Operator::Divide => {
+                    let n = get_param(&mut stack);
+                    get_param(&mut stack) / n
+                }
+                Operator::Mod => {
+                    let n = get_param(&mut stack) as u64;
+                    ((get_param(&mut stack) as u64) % n) as f64
+                }
                 Operator::Negate => -get_param(&mut stack),
-                Operator::Power => get_param(&mut stack).powf(get_param(&mut stack))
+                Operator::Power => {
+                    let n = get_param(&mut stack);
+                    get_param(&mut stack).powf(n)
+                }
             },
             _ => 0.0
         };
